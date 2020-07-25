6133aab7de155a62967eb6dcae5b03e73daff584
zimablizko <nklv92@gmail.com>
2020-07-26 00:07:04 +0300

    fix2?

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__, static_folder='static', static_url_path='/static')
 app.config.from_object(Config)
 
 
