cd9ad631c78f5bd1acf449be9a86570aae00a3b3
zimablizko <nklv92@gmail.com>
2020-11-23 12:35:11 +0300

    фикс материалов

--- app/models.py
+++ app/models.py
@@ -183,12 +183,13 @@ class User(UserMixin, db.Model):
 
     def get_global_materials(self, type_id):
         return Material.query.filter_by(type_id=type_id).filter(Material.delete_date == None).order_by('id')
-    # def get_all_materials(self):
-    #     return Material.query.filter_by(user_id=self.id).filter(Material.delete_date == None).order_by(
-    #         desc('edit_date'))
-    #
-    # def get_glazes(self):
-    #     return self.get_all_materials().filter_by(type_id=1)
+
+    def get_all_materials(self):
+        return Material.query.filter_by(user_id=self.id).filter(Material.delete_date == None).order_by(
+            desc('edit_date'))
+
+    def get_glazes(self):
+        return self.get_all_materials().filter_by(type_id=1)
 
     @login.user_loader
     def load_user(id):
